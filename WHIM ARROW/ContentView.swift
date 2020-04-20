a0a82c978240bbf07472f5f99ac272034c7c1ecc
sayuroy <balotje918@gmail.com>
2020-04-20 12:54:39 +0900

    Initial Commit

---
+++ WHIM ARROW/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WHIM ARROW
+//
+//  Created by 堀越さゆり on 2020/04/20.
+//  Copyright © 2020 sayuri horikoshi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
